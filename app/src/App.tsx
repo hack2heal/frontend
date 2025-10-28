d61865bb5f50443b680df8965bac716ffeca03b2
Valeriia Zvezdina <83537406+valeriiazvezdina@users.noreply.github.com>
2025-10-28 23:44:34 +0100

    basic-app-implementation (#2)

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import BottomNavBar from './bottom-nav-bar/BottomNavBar';
+import TopBar from './top-bar/TopBar';
+
+export default function App() {
+	return (
+		<>
+			<TopBar />
+			<BottomNavBar />
+		</>
+	);
+}
